1c797c227ce4d504eeec0b472209aff635c23cf3
Luis DeLeon <luismdeleon@gmail.com>
2020-12-31 15:16:22 -0800

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,29 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    // directory name: 'build directory'
+    public: '/',
+    src: '/dist',
+  },
+  plugins: [
+    '@snowpack/plugin-svelte'
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<!-- App.svelte -->
+<script>
+  import { onMount } from 'svelte';
+
+  let count = 0;
+
+  onMount(() => {
+    const interval = setInterval(() => count++, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+<style>
+  /* css will go here */
+</style>
+<div class="App">
+  <header class="App-header">
+    Starter App...
+    <p>Page has been open for <code>{count}</code> seconds.</p>
+  </header>
+</div>
